f8c42d639dcac9f94bc7b2d81836f8d2f659386f
AdithyanValloor <165929678+AdithyanValloor@users.noreply.github.com>
2025-02-13 21:17:15 +0530

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-gray-800 h-[400px] w-full'>
-      
+    <div className='bg-black h-[100px] w-full flex items-center justify-center'>
+      <p className='font-silkscreen text-white font-light'><span className='font-poppins'>© 2025</span> | Virtumart</p>
     </div>
   )
 }
