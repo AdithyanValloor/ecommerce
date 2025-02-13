4493be140346c649fd42c5427589e1e4848ff15d
AdithyanValloor <165929678+AdithyanValloor@users.noreply.github.com>
2025-02-13 21:02:20 +0530

    Errors fixed

--- src/components/ProductsCall.jsx
+++ src/components/ProductsCall.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backendURL = 'http://localhost:5000' || import.meta.env.VITE_BACKEND_URL
+const backendURL = import.meta.env.VITE_BACKEND_URL
 
 export const getProducts = async (category) => {
     try {
